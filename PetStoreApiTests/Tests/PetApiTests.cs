abaeaeeb2c87dfb23dbef60a9499c0e158549a68
Syed Abdul Hassan <SyedAbdul.Hassan@kpmg.co.uk>
2025-05-27 14:40:04 +0100

    minor refactoring

--- PetStoreApiTests/Tests/PetApiTests.cs
+++ PetStoreApiTests/Tests/PetApiTests.cs
@@ -64,9 +64,7 @@ namespace PetStoreApiTests.Tests
         {
             Assume.That(_petId, Is.GreaterThan(0));
 
-            var response = PollUntilSuccess($"pet/{_petId}");
-
-            Thread.Sleep(5000);
+            var response = PollUntilSuccess($"pet/{_petId}", Method.Get);
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
             var pet = JsonSerializer.Deserialize<Pet>(response.Content, new JsonSerializerOptions
@@ -80,10 +78,7 @@ namespace PetStoreApiTests.Tests
         public void DeletePet_ShouldSucceed()
         {
             Assume.That(_petId, Is.GreaterThan(0));
-
-            var request = new RestRequest($"pet/{_petId}", Method.Delete);
-            var response = _client.Execute(request);
-            Thread.Sleep(5000);
+            var response = PollUntilSuccess($"pet/{_petId}", Method.Delete);
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
 
@@ -91,9 +86,7 @@ namespace PetStoreApiTests.Tests
         public void GetDeletedPet_ShouldReturnNotFound()
         {
             Assume.That(_petId, Is.GreaterThan(0));
-
-            var request = new RestRequest($"pet/{_petId}", Method.Get);
-            var response = _client.Execute(request);
+            var response = PollUntilSuccess($"pet/{_petId}", Method.Get);
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
         }
 
@@ -104,13 +97,13 @@ namespace PetStoreApiTests.Tests
             public string status { get; set; }
         }
 
-        private RestResponse PollUntilSuccess(string resource, int maxAttempts = 5, int delayMs = 1000)
+        private RestResponse PollUntilSuccess(string resource,Method method, int maxAttempts = 5, int delayMs = 3000)
         {
             RestResponse response = null;
 
             for (int i = 0; i < maxAttempts; i++)
             {
-                var request = new RestRequest(resource, Method.Get);
+                var request = new RestRequest(resource, method);
                 response = _client.Execute(request);
 
                 if (response.StatusCode == HttpStatusCode.OK)
